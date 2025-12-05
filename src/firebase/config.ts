063c1a8714921e8559537e02b68d70814fb5ee42
Samson Jr Namanya <samsonjrnamanya1627@gmail.com>
2025-12-05 15:47:21 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-658202948-c3764",
+  "appId": "1:919643326214:web:2037847c3f5b94e23f8087",
+  "apiKey": "AIzaSyBBnlSYbmEus5k8a7f2AvpOGZug-3TX8qo",
+  "authDomain": "studio-658202948-c3764.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "919643326214"
+};
